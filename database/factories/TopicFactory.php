37ac7068b940197b140bbe9bc6e7ee09b932c76b
vagrant <vagrant@homestead>
2018-12-28 15:53:02 +0000

    添加话题列表的数据填充

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,17 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+
+    $sentence = $faker->sentence();
+
+    //随机取一个月以内的时间
+    $update_at = $faker->dateTimeThisMonth();
+    $create_at = $faker->dateTimeThisMonth($update_at);
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $create_at,
+        'updated_at' => $update_at,
     ];
 });
